ace89e72374ab2e02b782715111e71e0a3647bc6
Evgeniy <terrestrus@gmail.com>
2016-11-10 10:27:21 +0300

    task b

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
+
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
@@ -8,6 +9,33 @@ app.get('/', (req, res) => {
     hello: 'JS World',
   });
 });
+app.get('/task2B', (req, res) => {
+
+  let name = '';
+
+ let n = req.query.fullname.split(" ");
+ if (n.length > 3) {
+   res.send('Invalid fullname');
+ }
+  name = n[n.length - 1] + " ";
+  if (n.length == 3) {
+    for (let i = 0; i < 2; i++) {
+        console.log(n[i]);
+       name += n[i].slice(0,1)+"."+ " ";
+  }
+}
+  if (n.length == 2) {
+    for (let i = 0; i < 1; i++) {
+        console.log(n[i]);
+       name += n[i].slice(0,1)+"."+ " ";
+  }
+}
+
+  console.log(n);
+  res.send(name);
+
+});
+
 
 app.get('/task2A', (req, res) => {
 
